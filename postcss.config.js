8a59db881ebc3f2594b72586c5c0fc54ae150493
Henri Binsztok <808274+hbbio@users.noreply.github.com>
2024-04-18 14:26:24 +0800

    package: build-css

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
+import autoprefixer from "autoprefixer"
+import cssnano from "cssnano"
+import tailwindcss from "tailwindcss"
+
 export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {}
-  }
+  plugins: [
+    tailwindcss, autoprefixer, cssnano
+  ]
 };
