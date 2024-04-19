4b779e619a3e66157e739e08bd76392a43c05420
Henri Binsztok <808274+hbbio@users.noreply.github.com>
2024-04-19 15:27:24 +0800

    index: cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,113 +1,90 @@
-export { default as Button } from "./Button.svelte";
-
-export { default as Icon } from "./Icon.svelte";
-
-export { default as Loading } from "./Loading.svelte";
-
-export { default as Dropdown } from "./Dropdown.svelte";
-export { default as InlineHelpText } from "./InlineHelpText.svelte";
-
-export { default as InputText } from "./InputText.svelte";
-
-export { default as Checkbox } from "./Checkbox.svelte";
-export { default as Calendar } from "./Calendar.svelte";
-
-export { default as Range } from "./Range.svelte";
-
-export { default as Tooltip } from "./Tooltip.svelte";
-
-export { default as MasterDetail } from "./MasterDetail.svelte";
-export { default as SortableList } from "./SortableList.svelte";
-
-export { default as Card } from "./Card.svelte";
-
-export { default as QRCode } from "./QRCode.svelte";
-export { default as Hoverable } from "./Hoverable.svelte";
-
-export { default as PrettyDate } from "./PrettyDate.svelte";
-export { default as CustomSelect } from "./CustomSelect.svelte";
-export { default as Collapsible } from "./Collapsible.svelte";
-
-export { default as InlineDescription } from "./InlineDescription.svelte";
-
-export { default as Price } from "./Price.svelte";
-export { default as PercentSnippet } from "./PercentSnippet.svelte";
-export { default as Change } from "./Change.svelte";
 export { default as ActiveStatus } from "./ActiveStatus.svelte";
 export { default as AlertBanner } from "./AlertBanner.svelte";
-
+export { default as Button } from "./Button.svelte";
+export { default as Calendar } from "./Calendar.svelte";
+export { default as Card } from "./Card.svelte";
+export { default as Change } from "./Change.svelte";
+export { default as Checkbox } from "./Checkbox.svelte";
+export { default as Collapsible } from "./Collapsible.svelte";
+export { default as CustomSelect } from "./CustomSelect.svelte";
+export { default as Dropdown } from "./Dropdown.svelte";
+export { default as Hoverable } from "./Hoverable.svelte";
+export { default as Icon } from "./Icon.svelte";
+export { default as InlineDescription } from "./InlineDescription.svelte";
+export { default as InlineHelpText } from "./InlineHelpText.svelte";
+export { default as InputText } from "./InputText.svelte";
+export { default as Loading } from "./Loading.svelte";
+export { default as MasterDetail } from "./MasterDetail.svelte";
+export { default as PercentSnippet } from "./PercentSnippet.svelte";
+export { default as PrettyDate } from "./PrettyDate.svelte";
+export { default as Price } from "./Price.svelte";
+export { default as QRCode } from "./QRCode.svelte";
+export { default as Range } from "./Range.svelte";
+export { default as SimpleModal } from "./SimpleModal.svelte";
+export { default as SortableList } from "./SortableList.svelte";
 export { default as Tab } from "./Tab.svelte";
+export { type TabComponent } from "./Tab.utils";
 export { default as TabList } from "./TabList.svelte";
 export { default as TabPanel } from "./TabPanel.svelte";
-
-export { default as TabsMain } from "./TabsMain.svelte";
 export { default as TabWrapper } from "./TabWrapper.svelte";
-
-export { default as SimpleModal } from "./SimpleModal.svelte";
-
-export { type IconName, paths24solid, paths24outline } from "./icons";
-
-export { type TabComponent } from "./Tab.utils";
-
+export { default as TabsMain } from "./TabsMain.svelte";
 export { default as Toggle } from "./Toggle.svelte";
-
-export type {
-  FillStyle,
-  TextStyle,
-  ButtonStyle,
-  ButtonSize,
-  CheckboxSize,
-  CheckboxStyle,
-  InputStyle,
-  InputSize,
-  LabelStyle,
-  DropdownStyle,
-  DropdownSize,
-  TabStyle,
-  TabSize,
-  TabMargin,
-  TooltipStyle,
-  TooltipPosition,
-  RangeStyle,
-  RangeSize
-} from "./ui";
-
+export { default as Tooltip } from "./Tooltip.svelte";
+export { paths24outline, paths24solid, type IconName } from "./icons";
+export { Theme, ThemeContextKey, getColor, getTheme } from "./theme/theme";
 export {
-  textStyles,
-  buttonStyles,
-  buttonSizes,
-  checkboxSizes,
-  checkboxStyles,
-  inputStyles,
-  inputSizes,
-  labelStyles,
-  dropdownStyles,
-  dropdownSizes,
-  tabStyles,
-  tabSizes,
-  tabMargins,
-  tooltipPositions,
-  tooltipStyles,
-  rangeStyles,
-  rangeSizes
-} from "./ui";
-
-export { ThemeContextKey, Theme, getTheme, getColor } from "./theme/theme";
-
-export {
-  ThemeBackgroundTransparent,
-  ThemeFocus,
-  ThemeError,
-  ThemeText,
-  ThemeDark,
-  ThemeBackground,
-  ThemeButton,
   ThemeAccent,
-  type ThemeExtras,
-  type ThemeParts,
+  ThemeBackground,
+  ThemeBackgroundTransparent,
+  ThemeButton,
+  ThemeDark,
+  ThemeError,
+  ThemeFocus,
+  ThemeText,
   type CompiledThemeValue,
   type RawThemeDefinition,
   type ThemeDefinition,
-  type ThemeProperty,
-  type ThemeElementType
+  type ThemeElementType,
+  type ThemeExtras,
+  type ThemeParts,
+  type ThemeProperty
 } from "./theme/types";
+export {
+  buttonSizes,
+  buttonStyles,
+  checkboxSizes,
+  checkboxStyles,
+  dropdownSizes,
+  dropdownStyles,
+  inputSizes,
+  inputStyles,
+  labelStyles,
+  rangeSizes,
+  rangeStyles,
+  tabMargins,
+  tabSizes,
+  tabStyles,
+  textStyles,
+  tooltipPositions,
+  tooltipStyles
+} from "./ui";
+export type {
+  ButtonSize,
+  ButtonStyle,
+  CheckboxSize,
+  CheckboxStyle,
+  DropdownSize,
+  DropdownStyle,
+  FillStyle,
+  InputSize,
+  InputStyle,
+  LabelStyle,
+  RangeSize,
+  RangeStyle,
+  TabMargin,
+  TabSize,
+  TabStyle,
+  TextStyle,
+  TooltipPosition,
+  TooltipStyle
+} from "./ui";
