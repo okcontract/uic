efd984492754692db688a682bd7a08aa36ac744a
Henri Binsztok <808274+hbbio@users.noreply.github.com>
2024-05-24 14:46:24 +0800

    Dropdown: rewrite

--- src/Dropdown.svelte
+++ src/Dropdown.svelte
@@ -5,7 +5,6 @@
 
   import { getTheme } from "./theme/theme";
   import { ThemeBackground, ThemeText } from "./theme/types";
-
   import {
     dropdownSizes,
     dropdownStyles,
@@ -16,22 +15,16 @@
   const dispatch = createEventDispatcher();
 
   export let buttonElement: HTMLElement;
-  export let dropdownOpen: AnyCell<boolean>;
+  export let open: AnyCell<boolean>;
   export let style: DropdownStyle = "bottom";
   export let size: DropdownSize = "sm";
-  export let split = false;
 
   const theme = getTheme();
   const compiledTheme = theme?.compiled;
 
-  let menuElement: HTMLElement;
-
   const handleWindowClick = (ev: MouseEvent) => {
-    const path = ev.composedPath(); // ev.path on old browsers?
-    if (path.includes(buttonElement) || path.includes(menuElement)) {
-      // Do not close menu when clicking on button or menu.
-      return;
-    }
+    const path = ev.composedPath();
+    if (path.includes(buttonElement)) return;
     dispatch("close", "click");
   };
   const handleKeyDown = (ev: KeyboardEvent) => {
@@ -45,14 +38,13 @@
 
 <details
   class="dropdown {dropdownStyles[style]}"
-  open={$dropdownOpen instanceof Error ? false : $dropdownOpen}
+  open={$open instanceof Error ? false : $open}
 >
-  <summary>
+  <summary class="list-none">
     <slot name="action" />
   </summary>
   <div
-    bind:this={menuElement}
-    class="flex dropdown-content justify-center z-[1] shadow rounded-box {dropdownSizes[
+    class="dropdown-content z-[1] menu p-2 shadow rounded-box {dropdownSizes[
       size
     ]} {theme.dark(
       $compiledTheme,
@@ -63,8 +55,5 @@
     style={theme.apply($compiledTheme, [ThemeBackground, ThemeText])}
   >
     <slot name="main" />
-    {#if split}
-      <slot name="additional" />
-    {/if}
   </div>
 </details>
