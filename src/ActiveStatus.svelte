79275c89a14ad0b15c0c0d326b16805e2320c8e6
Henri Binsztok <808274+hbbio@users.noreply.github.com>
2024-12-19 13:31:52 +0800

    ActiveStatus, Button: title support

--- src/ActiveStatus.svelte
+++ src/ActiveStatus.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   export let act: boolean;
   export let label: string = undefined;
+  export let title = undefined;
 </script>
 
 <div
@@ -8,5 +9,5 @@
     ? 'badge-md'
     : 'badge-xs'}"
 >
-  {#if label}<span>{label}</span>{/if}
+  {#if label}<span {title}>{label}</span>{/if}
 </div>
