6f243fd60ea71a9d094dde14cf7d43e3429c865d
Ida Swarczewskaja <creatrice@gmail.com>
2025-01-12 22:57:19 +0800

    ActiveStatus: add outline

--- src/ActiveStatus.svelte
+++ src/ActiveStatus.svelte
@@ -2,12 +2,13 @@
   export let act: boolean;
   export let label: string = undefined;
   export let title = undefined;
+  export let outline: boolean;
 </script>
 
 <div
-  class="badge {act ? 'badge-success' : 'badge-error'} {label
-    ? 'badge-md'
-    : 'badge-xs'}"
+  class="badge {act ? 'badge-success' : 'badge-error'}{outline
+    ? ' badge-outline'
+    : ''} {label ? 'badge-md' : 'badge-xs'}"
 >
   {#if label}<span {title}>{label}</span>{/if}
 </div>
